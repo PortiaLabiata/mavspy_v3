93531bc32c8e5096adb3eb935ea59de2399a1182
Ilya Filippov <themorgot@yandex.ru>
2026-02-04 15:28:08 +0300

    Started to implement command line options

---
+++ help.hpp
@@ -0,0 +1,12 @@
+#ifndef HELP_H
+#define HELP_H
+
+#include <string>
+std::string help_msg = "MAVSpy v3.0. Help:\n"
+                       "--interface\tSelects network interface, defaults to local loop\n"
+                       "--help, -h\tPrint this message and exit.\n"
+                       "--print-socks\tPrint sockets that messages originate from\n"
+                       "--print-fields\tPrint fields' values\n";
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -3,8 +3,23 @@
 #include "capture_backend_udp.hpp"
 #include "ui_utility.hpp"
 
+#include "help.hpp"
+#include "argh.h"
+
 int main(int argc, char **argv) {
-    capture::backend_udp_t back("lo");
+    auto parser = argh::parser(argc, argv);
+
+    capture::backend_udp_t back(
+        parser("interface", "lo").str().c_str()
+    );
+    
+    if (parser[{"-h", "--help"}]) {
+        std::cout << help_msg;
+        std::exit(0);
+    }
+    bool print_fields = parser["print-fields"];
+    bool print_socks = parser["print-socks"];
+
     if (back.is_err()) {
         std::cout << "Could not init pcap" 
                   << std::endl;
@@ -14,10 +29,14 @@ int main(int argc, char **argv) {
     while (1) {
         if (back.listen()) {
             auto& pkt = back.data().back();
-            std::cout << ui::print_packet_sock(pkt)
-                      << ui::print_packet_message(pkt)
-                      << ui::print_packet_fields(pkt)
-                      << std::endl;
+            if (print_socks)
+                std::cout << ui::print_packet_sock(pkt);
+
+            std::cout << ui::print_packet_message(pkt);
+
+            if (print_fields)
+                std::cout << ui::print_packet_fields(pkt);
+            std::cout << std::endl;
         }
     }
     return 0;
